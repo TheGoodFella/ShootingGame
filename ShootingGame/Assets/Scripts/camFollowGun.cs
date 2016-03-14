d3d746634184b902537c8486a41fe647ad4a6c66
TheGoodFella <galas.daniele@gmail.com>
2016-03-14 19:38:41 +0100

    rewriting moving gun and camera scripts

---
+++ ShootingGame/Assets/Scripts/camFollowGun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camFollowGun : MonoBehaviour {
+
+    public GameObject gun;
+    Vector3 difference;
+
+	// Use this for initialization
+	void Start ()
+    {
+        difference = transform.position - gun.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = gun.transform.position + difference;
+	}
+}
