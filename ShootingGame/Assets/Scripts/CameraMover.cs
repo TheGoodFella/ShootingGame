727e71f0b540a60653cc6cef35f705cef946f9fe
thegoodfella <galas.daniele@gmail.com>
2016-03-14 16:00:40 +0100

    move camera and gun forward and backwards with W and S keys

---
+++ ShootingGame/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+    private Vector3 offset;
+    public float sensivity = 10;
+    private Vector3 v3;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        v3 = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * sensivity);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.forward * -sensivity);
+    }
+}
