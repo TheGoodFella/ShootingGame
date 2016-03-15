dea3456913e1463f68615fccbfa5c928a27239fc
TheGoodFella <galas.daniele@gmail.com>
2016-03-15 18:25:04 +0100

    create script start the fire when press mouse left

---
+++ ShootingGame/Assets/Scripts/mouseEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseEvents : MonoBehaviour {
+
+    public ParticleSystem fireParticles;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))  //left mouse btn
+        {
+            Debug.Log("Pressed mouse button left - 0");
+
+            if (!fireParticles.isPlaying)
+                fireParticles.Play();
+        }
+
+        if (Input.GetMouseButtonDown(1))  //right mouse btn
+        {
+            Debug.Log("Pressed mouse button right - 1");
+        }
+
+        if (Input.GetMouseButtonDown(2))  //middle mouse btn
+        {
+            Debug.Log("Pressed mouse button middle - 2");
+        }
+    }
+}
