65371d6f1f527e31fe8ebd50b3f20d74885aee57
thegoodfella <galas.daniele@gmail.com>
2016-03-17 09:59:45 +0100

    switch mouse locking and esc key press to new script in MAIN object

---
+++ ShootingGame/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+    /// <summary>
+    /// check if the cursor was locked or unlocked when we press esc
+    /// </summary>
+    private bool isCursorLocked;
+
+	// Use this for initialization
+    void Start()
+    {
+        //on start lock the cursor to the center of the game window and hide it
+        SetCursorState(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if we press esc the cursor will unlock from the center of game window and it became visible if it wasn't, otherwise it again becomes locked and hidden
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isCursorLocked)
+                SetCursorState(false);
+            else
+                SetCursorState(true);
+        }
+	}
+
+    /// <summary>
+    /// set the state of the cursor: lock to the gamewindow and hide it if the doLock is true, otherwise unlock and show
+    /// </summary>
+    /// <param name="doLock">I have to lock and hide cursor?</param>
+    void SetCursorState(bool doLock)
+    {
+        if (doLock)  //lock and hide cursor
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            isCursorLocked = true;
+        }
+        else  //unlock and show cursor
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            isCursorLocked = false;
+        }
+    }
+}
