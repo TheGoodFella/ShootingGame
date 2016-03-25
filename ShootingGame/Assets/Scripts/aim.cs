3d23d4b9eaabbbd7fc09958a6c38cd4be82ff44b
TheGoodFella <galas.daniele@gmail.com>
2016-03-25 16:34:00 +0100

    add raw aim animation

---
+++ ShootingGame/Assets/Scripts/aim.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aim : MonoBehaviour {
+    
+    private bool isAimed = false;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (!isAimed)
+                GetComponent<Animation>().Play();
+            isAimed = true;
+        }
+        else
+        {
+            if (isAimed)
+            {
+                isAimed = false;
+                GetComponent<Animation>().Play();
+            }
+        }
+    }
+}
