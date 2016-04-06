6391dd3b1242337bbf8249f1766294f4573f2a23
TheGoodFella <galas.daniele@gmail.com>
2016-04-06 18:34:53 +0200

    resolve mindfuck boolean mute/unmute variables

--- ShootingGame/Assets/Scripts/mute.cs
+++ ShootingGame/Assets/Scripts/mute.cs
@@ -1,21 +1,41 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class mute : MonoBehaviour
 {
-    public bool muteBool = false;
+    /// <summary>
+    /// Reproduce sounds? (yes:true/no:false)
+    /// </summary>
+    public bool muteSounds = false;
     public AudioSource[] audios;
+    public Text muteTXT;
+
+    void Start()
+    {
+        updateMute();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            muteBool = !muteBool;
+            muteSounds = !muteSounds;
 
-            Debug.Log("muteBool:" + muteBool);
-
-            for (int i = 0; i < audios.Length; i++)
-                audios[i].mute = muteBool;
+            updateMute();
         }
+    }
+
+    void updateMute()
+    {
+        Debug.Log("mute sounds:" + muteSounds);
+
+        if (muteSounds)
+            muteTXT.text = "unmute (M)";
+        else
+            muteTXT.text = "mute (M)";
+
+        for (int i = 0; i < audios.Length; i++)
+            audios[i].mute = muteSounds;
     }
 }
