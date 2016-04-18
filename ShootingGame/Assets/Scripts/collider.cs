9af8970dbb281a2667ac0802d6ece5187b2b4988
thegoodfella <galas.daniele@gmail.com>
2016-04-18 16:12:00 +0200

    add destroy delay for smoke particles system when bullet touch something

--- ShootingGame/Assets/Scripts/collider.cs
+++ ShootingGame/Assets/Scripts/collider.cs
@@ -6,12 +6,17 @@ public class collider : MonoBehaviour {
     public GameObject particleSmoke;
 
     /// <summary>
+    /// how many seconds the particles smoke must stay alive?
+    /// </summary>
+    public float destroyDelaySeconds = 2;
+
+    /// <summary>
     /// destroy ball when collide with something
     /// </summary>
     /// <param name="col"></param>
     void OnCollisionEnter(Collision col)
     {
-        Instantiate(particleSmoke, GetComponent<Transform>().position, Quaternion.identity);
+        Destroy(Instantiate(particleSmoke, GetComponent<Transform>().position, Quaternion.identity), destroyDelaySeconds);
         Destroy(gameObject);
         //col.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000.0f, col.gameObject.GetComponent<Rigidbody>().position, 5.0f);
     }
