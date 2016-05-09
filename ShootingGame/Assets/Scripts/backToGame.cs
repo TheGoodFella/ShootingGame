647e8ed4e3df410c993217ef25fef1a63f51a3b1
thegoodfella <galas.daniele@gmail.com>
2016-05-09 15:54:38 +0200

    credits scene improve, switch between credits scene and scene1 (game scene)

---
+++ ShootingGame/Assets/Scripts/backToGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class backToGame : MonoBehaviour {
+
+    /// <summary>
+    /// back to the game, so load the game scene
+    /// </summary>
+    public void backToGameClick()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+}
---
+++ ShootingGame/Assets/Scripts/creditsTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditsTime : MonoBehaviour {
+
+    /// <summary>
+    /// the fire particles emitter
+    /// </summary>
+    public GameObject fireEmitter;
+    /// <summary>
+    /// speed time scale of the render of particle system
+    /// </summary>
+    public float timeScale = 0.15f;
+
+	// Use this for initialization
+	void Start () {
+        //apply the time scale to the particle system
+        fireEmitter.GetComponent<ParticleSystem>().playbackSpeed = timeScale;
+        //Time.timeScale = timeScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //is the particle already playing? if not, play it
+	    if(!fireEmitter.GetComponent<ParticleSystem>().isPlaying)
+            fireEmitter.GetComponent<ParticleSystem>().Play();
+	}
+}
